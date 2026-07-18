055f9e4af08926c0cfc85b0da654a1f615e352a7
MayurTank07 <mayurtank4321@gmail.com>
2026-07-18 18:37:20 +0530

    Fix service redirect and complete favicon support

--- src/pages/OurClientPage.jsx
+++ src/pages/OurClientPage.jsx
@@ -5,10 +5,23 @@ import TrustedClients from "../components/Client/TrustedClients";
 import TrustedOrganizations from "../components/Client/TrustedOrganizations";
 import Footer from "../components/Footer/Footer";
 
+const clientsSchema = {
+  "@context": "https://schema.org",
+  "@type": "CollectionPage",
+  "name": "Recruitment Clients Across India",
+  "description": ROUTE_SEO['/our-clients'].description,
+  "url": "https://www.talentgroupofindia.com/our-clients",
+  "isPartOf": {
+    "@type": "WebSite",
+    "name": "Talent Group of India",
+    "url": "https://www.talentgroupofindia.com"
+  }
+};
+
 export default function OurClientPage() {
   return (
     <div className="bg-gray-100 min-h-screen">
-      <SEO {...ROUTE_SEO['/our-clients']} />
+      <SEO {...ROUTE_SEO['/our-clients']} schema={clientsSchema} />
       <TrustedClients />
       <TrustedOrganizations />
       <Footer />
